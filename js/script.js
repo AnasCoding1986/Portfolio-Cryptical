f478005c515733e7e57a3049e159af046074dfbc
Anas <anassust.scw@gmail.com>
2023-12-09 03:53:44 +0600

    Resolving Contact color dark mode

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ var typed = new Typed('.typing',{
     backSpeed:70,
     loop:true
 })
+
+const asideAn = document.querySelectorAll(".aside-a");
+
+asideAn.forEach(asideA=>{
+    asideA.addEventListener('click', function () {
+        asideAn.forEach(asideA => asideA.classList.remove('active'));
+        this.classList.add('active'); 
+    });
+});
+
+// let buttons = document.querySelectorAll('.btn');
+//   console.log(buttons);
+//   buttons.forEach(button => {
+//   button.addEventListener('click', function () {
+//       buttons.forEach(btn => btn.classList.remove('active'));
+//       this.classList.add('active');        
+//   });
+// });

f9ffb36f090721c0b2867e57acdca1d9f2d03700
Anas <anassust.scw@gmail.com>
2023-12-08 10:26:48 +0600

    Finish With A portfolio bug

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ============Typing Animation============
+var typed = new Typed('.typing',{
+    strings:[' ', 'Investor', 'Freelancer', 'Banker'],
+    typeSpeed:100,
+    backSpeed:70,
+    loop:true
+})
